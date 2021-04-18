b58a930150c8c17c1f90fec78146314117f61531
Alessandra Lino <ale.lino.00@hotmail.com>
2021-04-18 11:00:32 -0300

    #10: Aplicando SRP - Single Responsibility  na classe Startup.cs

---
+++ src/DevIO.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection WebApiConfig(this IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                //estou suprimindo pq quero persoanlizar os erros retornados ao Cliente
+                options.SuppressModelStateInvalidFilter = true;
+            });
+
+            //abertura de api para utilização por aplicação cliente
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Development", builder =>
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            return services;
+        }
+
+
+        public static IApplicationBuilder UseMvcConfiguration(this IApplicationBuilder app)
+        {
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            return app;
+        }
+    }
+ }
--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -1,15 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DevIO.Api.Configuration;
 using DevIO.Data.Context;
 using Microsoft.EntityFrameworkCore;
@@ -35,25 +27,7 @@ namespace DevIO.Api
 
             services.AddAutoMapper(typeof(Startup));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                //estou suprimindo pq quero persoanlizar os erros retornados ao Cliente
-                options.SuppressModelStateInvalidFilter = true; 
-            });
-
-            //abertura de api para utilização por aplicação cliente
-            services.AddCors(options => 
-            {
-                options.AddPolicy("Development", builder => 
-                builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()); 
-            });
-
-         
+            services.WebApiConfig();
 
             services.ResolveDependencies();
         }
@@ -70,9 +44,7 @@ namespace DevIO.Api
                 app.UseHsts();
             }
 
-            app.UseCors("Development");
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvcConfiguration();
         }
     }
 }
